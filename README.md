f3b78acd6e2c2df8b919ba43334d847ab5376107
RuelOquindo <ruel@fivefish.net>
2017-05-24 19:32:00 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Eyedentity Eyecare
+
+Website files for Eyedentity Eyecare
+
+Eyedentity, P.C. 
+2176 Hillsboro Road
+Battlewood Shopping Center, Suite 100 
+Franklin TN 37069 
+Tel. 615.791.7030 
+info@eyedentityfranklin.com
---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+  // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+  $('.parallax').parallax();
+  $('.scrollspy').scrollSpy({
+    scrollOffset: 20
+  });
+});
+   
